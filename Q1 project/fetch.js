15d60b3ec3bd064be2c661860d594857aadbfc0a
Sanjeet Uppal <sanjeet.uppal92@gmail.com>
2017-02-03 17:02:11 -0800

    movie details function grabs right fetch but breaks

--- Q1 project/fetch.js
+++ Q1 project/fetch.js
@@ -1,23 +1,31 @@
- let url = "https://data.sfgov.org/resource/wwmu-gmzc.json";
- function getMovieJson(url){
+function Movie(jsonMovieObj){
+  this.title = jsonMovieObj.title;
+  this.locations = [jsonMovieObj.locations]
+}
+
+// let url = "https://data.sfgov.org/resource/wwmu-gmzc.json";
+function getMovieJson(url){
   return fetch(url)
   .then(function(promiseResponse){
-    return promiseResponse.json()
+  return promiseResponse.json()
   })
   .then(function(jsonResult){
     let movieDatabase = {};
+    // create array of keys from json result object
     for(let i = 0; i < jsonResult.length; i++) {
+      // build up an array of promises pass it into promise.all then
       let curMovie = jsonResult[i];
       if(movieDatabase[curMovie.title]){
-        movieDatabase[curMovie.title].locations.push(curMovie.locations)
         // push to movieDatabase[curMovie.title]'s location array
-      }
-      else {
-        movieDatabase[curMovie.title] = new Movie(curMovie)
+        movieDatabase[curMovie.title].locations.push(curMovie.locations)
+      } else {
         // make Movie obj and add it as value to movieDatabase
+        movieDatabase[curMovie.title] = new Movie(curMovie)
+        movieDetails(curMovie.title)
       }
     }
-    console.log(movieDatabase);
+    return movieDatabase;
+    //return promise.all
   })
   .catch(function(err){
     console.log(err);
@@ -25,9 +33,16 @@
 }
 getMovieJson(url)
 
-
-
-function Movie(jsonMovieObj){
-  this.title = jsonMovieObj.title;
-  this.locations = [jsonMovieObj.locations]
+// grab movie poster and plot
+function movieDetails(title){
+  let omdbApi = `http://omdbapi.com/?s=${title}`
+  console.log(omdbApi);
+  return fetch(omdbApi)
+  .then(function(movieResponse){
+    // console.log(movieResponse);
+    return movieResponse.json()
+  })
+  .then(function(movieObj){
+    // console.log(movieObj);
+  })
 }
